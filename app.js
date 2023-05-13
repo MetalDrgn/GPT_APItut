0c647a2fda3626e6c65909d7e7fe96712b65f86d
Ryan <22962262+MetalDrgn@users.noreply.github.com>
2023-05-13 14:51:55 -0700

    setup

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+const API_KEY = process.env.OPENAI_API_KEY
 async function fetchData() {
-  await fetch()
+  const response = await fetch("https://api.openai.com/v1/completions", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${API_KEY}`,
+      "Content-Type": "application/.json"
+    },
+    body: JSON.stringify({
+      model: "text.davinci-003",
+      prompt: "hello",
+      max_tokens: 10
+    })
+  })
+  const data = await response.json()
+  console.log(data)
 }
+
+fetchData()
