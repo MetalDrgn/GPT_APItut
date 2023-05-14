938f4a34b20e7568b15b8d1dd82ee420056d2e13
Ryan <22962262+MetalDrgn@users.noreply.github.com>
2023-05-14 10:17:41 -0700

    small progress

--- app.js
+++ app.js
@@ -15,5 +15,25 @@ async function fetchData() {
   const data = await response.json()
   console.log(data)
 }
+async function fetchDataChat() {
+  const response = await fetch("https://api.openai.com/v1/chat/completions", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${API_KEY}`,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [{
+        role: 'user',
+        content: 'hello'
+      }]
+      // prompt: "hello",
+      // max_tokens: 10
+    })
+  })
+  const data = await response.json()
+  console.log(data)
+}
 
-fetchData()
+fetchDataChat()
