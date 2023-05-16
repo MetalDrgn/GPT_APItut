83fd716853f6519eaf8f6f757206b1585027e057
Ryan <22962262+MetalDrgn@users.noreply.github.com>
2023-05-16 14:19:25 -0700

    Finished tut. improvements and fixes next.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,19 @@ function App() {
   const [value, setValue] = useState("");
   const [message, setMessage] = useState(null);
   const [chat, setChat] = useState([]);
-  const [title, setTitle] = useState([]);
+  const [title, setTitle] = useState("");
+
+  const createNewChat = () => {
+    setMessage(null);
+    setValue("");
+    setTitle("");
+  };
+
+  const handleClick = (uniqueTitle) => {
+    setTitle(uniqueTitle)
+    setMessage(null);
+    setValue("");
+  }
 
   const getMessages = async () => {
     const options = {
@@ -30,19 +42,42 @@ function App() {
     }
   };
 
-  useEffect( () => {
-    console.log(title, value, message)
+  useEffect(() => {
+    console.log(title, value, message);
     if (!title && value && message) {
-      setTitle(value)
+      setTitle(value);
+    }
+    if (title && value && message) {
+      setChat((chat) => [
+        ...chat,
+        {
+          title: title,
+          role: "user",
+          content: value,
+        },
+        {
+          title: title,
+          role: message.role,
+          content: message.content,
+        },
+      ]);
     }
   }, [message, title]);
+
+  console.log(chat);
+
+  const currentChat = chat.filter((chat) => chat.title === title);
+  const uniqueTitles = Array.from(new Set(chat.map((chat) => chat.title)));
+  console.log(uniqueTitles);
 
   return (
     <div className="App">
       <section className="side-bar">
-        <button>+ New chat</button>
+        <button onClick={createNewChat}>+ New chat</button>
         <ul className="history">
-          <li>ASD</li>
+          {uniqueTitles.map((uniqueTitle, index) => (
+            <li key={index} onClick={ () => handleClick(uniqueTitle)}>{uniqueTitle}</li>
+          ))}
         </ul>
         <nav>
           <p>Experimental</p>
@@ -50,21 +85,28 @@ function App() {
       </section>
       <section className="main">
         <h1>API_GPT</h1>
+        {/* {!title && <h1>API_GPT</h1>} */}
         <ul className="feed">
-          <div className="bottom-section">
-            <div className="input-container">
-              <input
-                type="text"
-                value={value}
-                onChange={(e) => setValue(e.target.value)}
-              />
-              <div id="submit" onClick={getMessages}>
-                {">"}
-              </div>
-            </div>
-            <p className="info">Chat GPT 3.5 using API</p>
-          </div>
+          {currentChat?.map((message, index) => (
+            <li key={index}>
+              <p className="role">{message.role}</p>
+              <p>{message.content}</p>
+            </li>
+          ))}
         </ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input
+              type="text"
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+            />
+            <div id="submit" onClick={getMessages}>
+              {">"}
+            </div>
+          </div>
+          <p className="info">Chat GPT 3.5 using API</p>
+        </div>
       </section>
     </div>
   );
